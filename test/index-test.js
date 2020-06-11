d18ac9e23daaba414ec1c5ab64ec1607ea964266
Gregory Singleton <yadig69design@gmail.com>
2020-06-11 22:39:57 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -8,7 +8,7 @@ describe('what-is-a-test', () => {
   })
 
 
-  describe('Height', (40) => {
+  describe('Height', () => {
     it('is less than 40', () => {
       expect(index.height).toBeLessThan(40)
     })
